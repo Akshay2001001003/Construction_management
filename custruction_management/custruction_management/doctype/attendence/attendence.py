9840032c4cd9f0e59ce6b6db3a92d880da7fa3a0
root <kumarakshay6979@gmail.com>
2024-06-06 12:34:19 +0530

    doctype

---
+++ custruction_management/custruction_management/doctype/attendence/attendence.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, shivansh akshay abhinav and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Attendence(Document):
+	pass
---
+++ custruction_management/custruction_management/doctype/site/test_site.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, shivansh akshay abhinav and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSite(FrappeTestCase):
+	pass
