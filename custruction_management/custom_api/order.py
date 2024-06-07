216238cc80d01e67f7c870164681261dbcb70a92
root <kumarakshay6979@gmail.com>
2024-06-07 16:47:41 +0530

    final

---
+++ custruction_management/custom_api/order.py
@@ -0,0 +1,32 @@
+import frappe
+from frappe import _
+
+@frappe.whitelist(allow_guest=True)
+def create_sales_order_via_rest_api(data):
+    try:
+        # Parse JSON data
+        data = frappe.parse_json(data)
+
+        # Create Sales Order
+        sales_order = frappe.get_doc({
+            "doctype": "Sales Order",
+            "customer": data.get("customer"),
+            "items": [
+                {
+                    "item_code": data.get("item_code"),
+                    # "description": data.get("description"),
+                    "rate": data.get("rate"),
+                    "qty": data.get("qty"),
+                    "delivery_date":data.get("delivery_date")
+                }
+            ]
+        })
+
+        # Save Sales Order
+        sales_order.insert(ignore_permissions=True)
+
+        return {"status": "success", "message": _("Sales Order created successfully")}
+    except Exception as e:
+        frappe.log_error(e)
+        print("Error------->",e)
+        return {"status": "error", "message": _("Failed to create Sales Order")}
